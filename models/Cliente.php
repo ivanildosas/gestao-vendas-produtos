f62f8cdd427b2164987919c504f42840578bb362
Ivanildo Souza <ivanildosas@gmail.com>
2021-08-18 05:01:58 -0300

    Cria model Cliente

---
+++ models/Cliente.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cliente".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $email
+ * @property string $cpf
+ *
+ * @property Pedido[] $pedidos
+ */
+class Cliente extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cliente';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'email', 'cpf'], 'required'],
+            [['nome'], 'string', 'max' => 100],
+            [['email'], 'string', 'max' => 50],
+            [['cpf'], 'string', 'max' => 12],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'email' => 'Email',
+            'cpf' => 'Cpf',
+        ];
+    }
+
+    /**
+     * Gets query for [[Pedidos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPedidos()
+    {
+        return $this->hasMany(Pedido::className(), ['id_cliente' => 'id']);
+    }
+}
