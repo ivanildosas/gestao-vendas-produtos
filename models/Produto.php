f5dff92567b604ada67d749f7100a13430fe7ff5
Ivanildo Souza <ivanildosas@gmail.com>
2021-08-18 06:00:13 -0300

    Cria model Produto

---
+++ models/Produto.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "produto".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property float $preco
+ * @property int $quantidade
+ *
+ * @property PedidoProduto[] $pedidoProdutos
+ */
+class Produto extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'produto';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'preco', 'quantidade'], 'required'],
+            [['preco'], 'number'],
+            [['quantidade'], 'default', 'value' => null],
+            [['quantidade'], 'integer'],
+            [['nome'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'preco' => 'Preco',
+            'quantidade' => 'Quantidade',
+        ];
+    }
+
+    /**
+     * Gets query for [[PedidoProdutos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPedidoProdutos()
+    {
+        return $this->hasMany(PedidoProduto::className(), ['id_produto' => 'id']);
+    }
+}
